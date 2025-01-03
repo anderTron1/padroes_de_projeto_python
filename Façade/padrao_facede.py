daf54f5983a282bc48317a245bc45a16707afe4f
anderTron1 <andre-luizpiresguimaraes@outlook.com>
2025-01-03 16:44:45 -0300

    comentario sobre o padrão façade

--- Façade/padrao_facede.py
+++ Façade/padrao_facede.py
@@ -1,5 +1,31 @@
 
+"""
+O padrão Façade (ou Fachada) é um padrão de projeto estrutural que fornece uma interface simplificada e 
+unificada para um conjunto de interfaces em um subsistema. Ele tem como objetivo reduzir a complexidade e 
+aumentar a legibilidade do código, tornando um subsistema mais fácil de usar.
 
+Características do Padrão Façade
+Interface Simplificada:
+
+Oculta os detalhes complexos de um subsistema, expondo apenas o que é essencial para o cliente.
+Encapsulamento de Complexidade:
+
+Reduz o número de objetos que o cliente precisa manipular diretamente.
+Maior Manutenibilidade:
+
+As mudanças internas do subsistema não afetam diretamente os clientes que usam a fachada.
+
+Quando Usar o Padrão Façade?
+Simplificar Interfaces Complexas:
+
+Quando um subsistema possui muitas classes ou interfaces que são difíceis de entender ou usar diretamente.
+Reduzir Acoplamento:
+
+Quando se deseja desacoplar o cliente da implementação detalhada de um subsistema.
+Organizar Sistemas Grandes:
+
+Quando há necessidade de estruturar subsistemas em camadas e expor apenas interfaces simplificadas para cada camada.
+"""
 class GerenciamentoEventos:
     def __init__(self):
         print("GerencimentoEventos :: Vou organizar com todo mundo!\n")
